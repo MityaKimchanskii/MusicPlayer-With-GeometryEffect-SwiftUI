7bb595c1311e4ecf87ba3b8ef2de1b8711d84498
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 23:47:51 -0700

    Initial Commit

---
+++ MusicPlayer-With-GeometryEffect-SwiftUI/MusicPlayer_With_GeometryEffect_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayer_With_GeometryEffect_SwiftUIApp.swift
+//  MusicPlayer-With-GeometryEffect-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayer_With_GeometryEffect_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
