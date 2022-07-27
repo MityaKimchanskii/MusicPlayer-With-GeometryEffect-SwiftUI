ce45115eae91c78f10c3a3c32c0ce8f77a18b5c4
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-27 00:20:14 -0700

    final commit

--- MusicPlayer-With-GeometryEffect-SwiftUI/ContentView.swift
+++ MusicPlayer-With-GeometryEffect-SwiftUI/ContentView.swift
@@ -7,10 +7,111 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MusicPlayerBar: View {
+    
+    let namespace: Namespace.ID
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Image("image")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .cornerRadius(3)
+                .padding()
+                .matchedGeometryEffect(id: "animation", in: namespace)
+            
+            Text("Down to Love")
+                .font(.headline)
+            
+            Spacer()
+            
+            Image(systemName: "play.fill")
+    
+            Image(systemName: "forward.fill")
+                .padding(.trailing, 10)
+   
+        }.frame(maxWidth: .infinity, maxHeight: 60)
+            .background(.white)
+    }
+}
+
+struct MusicPlayer: View {
+    
+    let namespace: Namespace.ID
+    
+    var body: some View {
+        VStack {
+            Image("image")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(20)
+                .padding(30)
+                .matchedGeometryEffect(id: "animation", in: namespace)
+            
+            HStack {
+                VStack{
+                    Text("Down to Love")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Text("Armin Van Burren")
+                        .font(.title)
+                        .fontWeight(.light)
+                        .opacity(0.5)
+                        .foregroundColor(.white)
+                }.padding()
+                Spacer()
+            }
+            
+            HStack {
+                Spacer()
+                
+                Image(systemName: "backward.fill")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30))
+                
+                Spacer()
+                
+                Image(systemName: "play.fill")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30))
+                
+                Spacer()
+                
+                Image(systemName: "forward.fill")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30))
+                
+                Spacer()
+            }
+            
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.yellow)
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var showDetails: Bool = false
+    @Namespace private var ns
+    
+    var body: some View {
+        
+        VStack {
+            
+            Spacer()
+            
+            if showDetails {
+                MusicPlayer(namespace: ns)
+            } else {
+                MusicPlayerBar(namespace: ns)
+            }
+            
+        }.onTapGesture {
+            withAnimation(.spring()) {
+                showDetails.toggle()
+            }
+        }
     }
 }
 
